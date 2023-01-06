536a2d354b2359226c777bb2b3d0b414ff9b9713
Timoteo Rider <Timoteo2.25@gmail.com>
2023-01-06 14:52:38 -0600

    Define mail struct

---
+++ zincindex/mail.go
@@ -0,0 +1,20 @@
+package zincindex
+
+type Mail struct {
+	MessageID        string `json:"message-id"`
+	Date             string `json:"date"`
+	From             string `json:"from"`
+	To               string `json:"to"`
+	Subject          string `json:"subject"`
+	MimeVersion      string `json:"mime-version"`
+	ContentType      string `json:"content-type"`
+	TransferEncoding string `json:"transfer-encoding"`
+	XFrom            string `json:"x-from"`
+	XTo              string `json:"x-to"`
+	XCC              string `json:"x-cc"`
+	XBCC             string `json:"x-bcc"`
+	XFolder          string `json:"x-folder"`
+	XOrigin          string `json:"x-origin"`
+	XFilename        string `json:"x-filename"`
+	Body             string `json:"body"`
+}
