4392b2aea6b665a16e525df98777e96a09e1dcc3
Timoteo Rider <Timoteo2.25@gmail.com>
2023-01-25 20:32:52 -0600

    Add flags to Envi for ZincSearch authorization

--- cmd/envi/main.go
+++ cmd/envi/main.go
@@ -15,6 +15,22 @@ Flags:
 	-profiler (optional)
 		Whether to have the profiler active or inactive.
 		Accepts the values "true" and "false".
+		By default, it's true
+
+	-address (optional)
+		The address of the ZincSearch server.
+		By default, it's "http://127.0.0.1:4080/".
+
+	-username (optional)
+		The username for the ZincSearch database.
+		By default, it's "admin"
+
+	-password (optional)
+		The password for the ZincSearch database.
+		By default, it's "admin"
+
+	-index (required)
+		The ZincSearch index to index the database into.
 
 	-help
 		Brings up a simple explanation of the program flags.
@@ -28,14 +44,23 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/logdot/zincSearch"
 	"net/http"
 	_ "net/http/pprof"
 )
 
+var zincSearchAuthentication *zincSearch.Authentication
+
 func main() {
+	username := flag.String("username", "admin", "The username for the ZincSearch server")
+	password := flag.String("password", "admin", "The password for the ZincSearch server")
+	index := flag.String("index", "", "The ZincSearch index to put the enron emails in")
 	addr := flag.String("address", ":8080", "The address to host the visualizer on")
+	zincAddr := flag.String("zinc address", "http://127.0.0.1:4080/", "The root address of the ZincSearch server")
 	profiler := flag.Bool("profiler", true, "To have the profiler enabled or disabled")
 	flag.Parse()
+
+	zincSearchAuthentication = zincSearch.Authenticate(*zincAddr, *index, *username, *password)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
