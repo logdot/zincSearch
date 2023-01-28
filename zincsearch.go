a8500b8379abca696786831f83994359d2cbed0f
Timoteo Rider <Timoteo2.25@gmail.com>
2023-01-28 02:34:27 -0600

    Update IndexDB to handle concurrent functions

--- zincsearch.go
+++ zincsearch.go
@@ -24,12 +24,12 @@ const (
 
 // IndexDB indexes the enron database at the given path and ingests it into ZincSearch.
 // path is expected to point at a directory containing `maildir/`.
-func (a *Authentication) IndexDB(path string) {
+func (a *Authentication) IndexDB(path string, concurrency uint, chunking uint) {
 	log.Println("Starting indexing")
 
-	mails := zincindex.IndexFolder(path)
+	mails := zincindex.IndexFolder(path, concurrency)
 
-	err := a.IngestBulk(mails, 10000)
+	err := a.IngestBulk(mails, chunking)
 
 	if err != nil {
 		log.Println(err)
